defb1e914f0fd8449a11bf5dac50763bc507d4c6
Bokele Wakiza Franck <bwakiza@gmail.com>
2022-06-29 10:03:21 +0200

    adding filter to payment

---
+++ app/Filament/Resources/PaymentResource/Pages/ListPayments.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Filament\Resources\PaymentResource\Pages;
+
+use App\Filament\Resources\PaymentResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListPayments extends ListRecords
+{
+    protected static string $resource = PaymentResource::class;
+
+    protected function getActions(): array
+    {
+        return [];
+    }
+}
